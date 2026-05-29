fa12bc6e3e3f4a20b68c5fb285d75e5d15430ac6
christianolivera45-oss <christian.olivera45@gmail.com>
2026-05-29 15:53:34 -0300

    Add files via upload

--- server.ts
+++ server.ts
@@ -1145,7 +1145,7 @@ async function startServer() {
     });
     app.use(vite.middlewares);
   } else {
-    const distPath = path.join(process.cwd(), "dist");
+    const distPath = path.join(process.cwd(), "dist/public");
     app.use(express.static(distPath));
     app.get("*", (req, res) => {
       res.sendFile(path.join(distPath, "index.html"));
